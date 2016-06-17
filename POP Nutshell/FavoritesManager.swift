77cc3e4b23da71d5bae1d906eac7a3d7ad88c0ee
Patrick <patrickbellot77@gmail.com>
2016-06-17 17:23:54 -0500

    added FavoritesManager

---
+++ POP Nutshell/FavoritesManager.swift
@@ -0,0 +1,23 @@
+//
+//  FavoritesManager.swift
+//  POP Nutshell
+//
+//  Created by Patrick Bellot & Thomas Hanning www.twitter.com/@hanning_thomas on 6/17/16.
+//  Copyright © 2016 Bell OS, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class FavoritesManager {
+    
+    var managedContext: NSManagedObjectContext!
+    
+    func addFavorites() {
+        
+    }
+    
+    func getAllFavorites() {
+        
+    }
+}
