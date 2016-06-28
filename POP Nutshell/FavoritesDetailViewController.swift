f249ecfb12a671fe7a9bfa51ebcc98dc8fe59ff8
Patrick <patrickbellot77@gmail.com>
2016-06-28 15:13:26 -0500

    corrected string error for being to complex for the little compiler because it's so fragile

--- POP Nutshell/FavoritesDetailViewController.swift
+++ POP Nutshell/FavoritesDetailViewController.swift
@@ -40,7 +40,7 @@ class FavoritesDetailViewController: UIViewController {
         // Adjust the height of the webView constraint
         self.webViewHeightConstraint.constant = height
         
-            let videoEmbedString =  "<html><head><style type=\"text/css\">body {background-color:transparent;color: white;}</style></head><body style=\"margin:0\"><iframe frameBorder=\"0\" height=\"" + String(height) + "\" width=\"" + String(width) + "\" src=\"http://www.youtube.com/embed/ " + favVid.videoId + " ?showinfo=0&modestbranding=1&frameborder=0&rel=0\"></iframe></body></html>"
+            let videoEmbedString =  "<html><head><style type=\"text/css\">body {background-color:transparent;color: white;}</style></head><body style=\"margin:0\"><iframe frameBorder=\"0\" height=\"" + String(height) + "\" width=\"" + String(width) + "\" src=\"http://www.youtube.com/embed/ " + String(favVid.videoId) + " ?showinfo=0&modestbranding=1&frameborder=0&rel=0\"></iframe></body></html>"
         
             webView.loadHTMLString(videoEmbedString, baseURL: nil)
         
