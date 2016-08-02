c0be9431cca1fc3ad51a463b6edeaa4f0bd8a9f5
Patrick <patrickbellot77@gmail.com>
2016-08-02 11:39:44 -0500

    added Reachability to PNSArticleController

--- POP Nutshell/PNSArticleController.swift
+++ POP Nutshell/PNSArticleController.swift
@@ -7,11 +7,62 @@
 //
 
 import UIKit
+import ReachabilitySwift
 
 class PNSArticleController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var articleView: UIWebView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var reachability: Reachability?
+    
+    override func viewWillAppear(animated: Bool) {
+        do {
+            reachability = try Reachability.reachabilityForInternetConnection()
+        } catch {
+            print("Unable to create Reachability")
+            return
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(PNSViewController.reachabilityChanged(_:)),name: ReachabilityChangedNotification,object: reachability)
+        do{
+            try reachability?.startNotifier()
+        }catch{
+            print("could not start reachability notifier")
+        }
+    }
+    
+    func reachabilityChanged(note: NSNotification) {
+        
+        let reachability = note.object as! Reachability
+        
+        if reachability.isReachable() {
+            if reachability.isReachableViaWiFi() {
+                print("Reachable via WiFi")
+            } else {
+                print("Reachable via Cellular")
+            }
+        } else {
+            print("Network not reachable")
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                print("Internet Unavailable")
+                
+                let alert = UIAlertController(title: "Internet Unavailable",
+                                              message: "Try again when connected to the Internet",
+                                              preferredStyle: .ActionSheet)
+                let okAction = UIAlertAction(title: "OK", style: .Default, handler: { (alert) in
+                    exit(0)
+                })
+                
+                alert.addAction(okAction)
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+        
+        reachability.stopNotifier()
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: ReachabilityChangedNotification, object: reachability)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
